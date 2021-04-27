d561886cc7bae7f8978d01cd54212de5f3f3d5f8
philipjackson <philiptgjackson@gmail.com>
2021-04-27 16:52:33 +0100

    feat: switched from ImageBitmap to b64, rendering images in material-ui Grid

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -3,44 +3,53 @@ import * as ReactDOM from "react-dom";
 
 import { UserInterface } from "@/index";
 
-
 // load the cells images, construct an array of tiles:
-const loadImage = (filename: string): Promise<ImageBitmap> =>
+const loadImage = (filename: string): Promise<string> =>
   new Promise((resolve) => {
     const image = new Image();
     image.crossOrigin = "anonymous";
 
     image.onload = () => {
-      createImageBitmap(image)
-        .then((imageBitmap) => {
-          console.log("Hello, World!")
-          console.log(resolve);
-          resolve(imageBitmap)
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
+      let b64 = canvas.toDataURL("image/png");
+      b64 = b64.replace("data:image/png;base64,", "");
+
+      resolve(b64);
     };
     image.src = filename;
   });
 
 // get image promises:
-const promises: Array<Promise<ImageBitmap>> = [];
-for (let i = 0; i < 20; i++) {
-  const filename = "cells/cell" + i + ".png";
-  promises.push(loadImage(filename).then((imageBitmap) => imageBitmap));
-} 
+const promises: Array<Promise<string>> = [];
+for (let i = 0; i < 20; i += 1) {
+  const filename = `cells/cell${i}.png`;
+  promises.push(loadImage(filename));
+}
 
 // unwrap promises and pass to curate:
-Promise.all(promises).then((images: ImageBitmap[]) => {  
-  // make tiles:
-  const tiles = images.map((image: ImageBitmap, i) => (
-    {name: "cell" + i + ".png", label: i % 2 ? "cancer" : "notcancer", thumbnail: image}
-  ))
-  // render main component:
-  ReactDOM.render(
-    <UserInterface tiles={tiles}/>,
-    document.getElementById("react-container")
-  )},
-  (e) => {console.log(e)}
-).catch((e) => {console.log(e)});
+Promise.all(promises)
+  .then(
+    (images: string[]) => {
+      // make tiles:
+      const tiles = images.map((image: string, i) => ({
+        name: `cells/cell${i}.png`,
+        label: i % 2 ? "cancer" : "notcancer",
+        thumbnail: image,
+      }));
+      // render main component:
+      ReactDOM.render(
+        <UserInterface tiles={tiles} />,
+        document.getElementById("react-container")
+      );
+    },
+    (e) => {
+      console.log(e);
+    }
+  )
+  .catch((e) => {
+    console.log(e);
+  });
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 
 export interface Tile {
   name: string;
   label: string;
-  thumbnail: ImageBitmap
+  thumbnail: string; // base64
 }
 
 interface Props {
   tiles: Array<Tile>;
 }
 
-export class UserInterface extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render = (): React.ReactNode => (
-    <div>
-      <h1>CURATE</h1>
-
-    </div>
-  )
-};
+export const UserInterface = (props: Props): React.ReactElement => (
+  <div>
+    <h1>CURATE</h1>
+    <Grid container spacing={3}>
+      {props.tiles.map((tile) => (
+        <Grid item xs={1}>
+          <img
+            height={128}
+            src={`data:image/png;base64,${tile.thumbnail}`}
+          ></img>
+        </Grid>
+      ))}
+    </Grid>
+  </div>
+);
