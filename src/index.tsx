8b6986e1bdd5e526be9bb7b96c9fa9382454ec2c
philipjackson <philiptgjackson@gmail.com>
2021-05-04 10:02:40 +0100

    Autoload (#15)
    * feat: add a small sample thumbnails dataset
    
    * feat: autoloading cells thumbnails, passing into UserInterface
    
    * feat: switched from ImageBitmap to b64, rendering images in material-ui Grid
    
    * fix: lint errors
    
    * fix: added keys to grid elements
    
    * fix: added id to tile interface, used as list key
    
    * fix: replaced cell images with branding image crops
    
    * chore: npm update
    
    Co-authored-by: Chas Nelson <chas@gliff.ai>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 
-export const UserInterface: React.FC = (): React.ReactElement => (
-  <h1>CURATE</h1>
+export interface Tile {
+  id: string;
+  name: string;
+  label: string;
+  thumbnail: string; // base64
+}
+
+interface Props {
+  tiles: Array<Tile>;
+}
+
+export const UserInterface = (props: Props): React.ReactElement => (
+  <div>
+    <h1>CURATE</h1>
+    <Grid container spacing={3}>
+      {props.tiles.map((tile) => (
+        <Grid item xs={1} key={tile.id}>
+          <img
+            height={128}
+            src={`data:image/png;base64,${tile.thumbnail}`}
+            alt={tile.name}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  </div>
 );
