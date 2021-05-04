ec67c2188ffab69a4e715830470e55c04cea965a
philipjackson <philiptgjackson@gmail.com>
2021-05-04 10:02:40 +0100

    Autoload (#15)
    * feat: add a small sample thumbnails dataset
    
    * feat: autoloading cells thumbnails, passing into UserInterface
    
    * feat: switched from ImageBitmap to b64, rendering images in material-ui Grid
    
    * fix: lint errors
    
    * fix: added keys to grid elements
    
    * fix: added id to tile interface, used as list key
    
    * fix: replaced cell images with branding image crops
    
    * chore: npm update
    
    Co-authored-by: Chas Nelson <chas@gliff.ai>

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -3,8 +3,54 @@ import * as ReactDOM from "react-dom";
 
 import { UserInterface } from "@/index";
 
-ReactDOM.render(
-  <UserInterface />,
+// load the sample images, construct an array of tiles:
+const loadImage = (filename: string): Promise<string> =>
+  new Promise((resolve) => {
+    const image = new Image();
+    image.crossOrigin = "anonymous";
 
-  document.getElementById("react-container")
-);
+    image.onload = () => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
+      let b64 = canvas.toDataURL("image/png");
+      b64 = b64.replace("data:image/png;base64,", "");
+
+      resolve(b64);
+    };
+    image.src = filename;
+  });
+
+// get image promises:
+const promises: Array<Promise<string>> = [];
+for (let i = 0; i < 20; i += 1) {
+  const filename = `samples/sample${i}.png`;
+  promises.push(loadImage(filename));
+}
+
+// unwrap promises and pass to curate:
+Promise.all(promises)
+  .then(
+    (images: string[]) => {
+      // make tiles:
+      const tiles = images.map((image: string, i) => ({
+        id: String(i),
+        name: `samples/sample${i}.png`,
+        label: i % 2 ? "cancer" : "notcancer",
+        thumbnail: image,
+      }));
+      // render main component:
+      ReactDOM.render(
+        <UserInterface tiles={tiles} />,
+        document.getElementById("react-container")
+      );
+    },
+    (e) => {
+      console.log(e);
+    }
+  )
+  .catch((e) => {
+    console.log(e);
+  });
