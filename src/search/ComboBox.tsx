94f54d20c17268171f36cde96aed21be32ee8bdd
Silvia Zeni <silvia.szn@gmail.com>
2021-05-05 14:25:13 +0100

    feat: Search by metadata and filter by label (#25)
    * chore: Add .vscode to .gitignore
    
    * chore: Add example for metadata
    
    * feat: Add autocomplete combobox for metadata key, value input
    
    * chore: Install @material-ui/lab
    
    * feat: Add class for handling operations on metadata (e.g., search)
    
    * chore: Add SearchClass to index.tsx
    
    * feat: Add navbar
    
    * feat: Add LabelsAccordion.tsx
    
    * feat: Change SearchClass.tsx into ui.tsx
    
    * chore: Update index.tsx (ui.tsx is now the entrypoint)
    
    * fix: ComboBox.tsx
    
    * chore: Simplify ComboBox.tsx
    
    * chore: Add items to metadata.json
    
    * feat: Apply selection to images
    
    * fix: Lint
    
    * fix: Examples not in staging area
    
    * chore: Remove commented out line
    
    Co-authored-by: Craig Cooper <c@c9r.dev>
    
    * fix: MetaItem in search/interfaces.ts
    
    Co-authored-by: Craig Cooper <c@c9r.dev>
    
    * fix: MetaItem is search/interfaces.ts (part2)
    
    * chore: Update src/search/ComboBox.tsx
    
    Co-authored-by: Craig Cooper <c@c9r.dev>
    
    * fix: Simplify code in LabelsAccordion.tsx
    
    * chore: Pin version for @material-ui/lab
    
    * fix: Desync between selected labels and images at load
    
    Co-authored-by: Craig Cooper <c@c9r.dev>

---
+++ src/search/ComboBox.tsx
@@ -0,0 +1,106 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { ChangeEvent, useState, useEffect, ReactElement } from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { IconButton, Paper, TextField } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { Search } from "@material-ui/icons";
+import { Metadata, MetaItem } from "./interfaces";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      right: "10px",
+      height: "85%",
+    },
+    input: {
+      paddingLeft: "10px",
+      width: "250px",
+    },
+    inputField: {
+      fontSize: "11px",
+    },
+    iconButton: {
+      padding: "10px",
+    },
+  })
+);
+
+interface Props {
+  metadata: Metadata;
+  metadataKeys: string[];
+  callback: (key: string, value: string) => void;
+}
+
+export default function ComboBox({
+  metadata,
+  metadataKeys,
+  callback,
+}: Props): ReactElement {
+  const style = useStyles();
+  const [inputKey, setInputKey] = useState("");
+  const [inputOptions, setOptions] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const updateOptions = (): void => {
+    if (!metadataKeys.includes(inputKey)) return;
+    const options: Set<string> = new Set();
+    metadata.forEach((mitem: MetaItem) => {
+      const value = mitem[inputKey];
+      if (Array.isArray(value)) {
+        value.forEach((v) => options.add(v));
+      } else {
+        options.add(value);
+      }
+    });
+    setOptions(Array.from(options));
+  };
+
+  useEffect(() => {
+    if (inputValue !== "") setInputValue("");
+    updateOptions();
+  }, [inputKey]);
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={(e) => {
+        callback(inputKey, inputValue);
+        e.preventDefault();
+      }}
+      className={style.root}
+    >
+      <Autocomplete
+        id="combobox-metadata-key"
+        className={style.input}
+        inputValue={inputKey}
+        onInputChange={(e: ChangeEvent, newInputKey: string) => {
+          setInputKey(newInputKey);
+        }}
+        options={metadataKeys}
+        renderInput={(params: any) => <TextField {...params} label="Key" />}
+      />
+      <Autocomplete
+        id="combobox-metadata-value"
+        className={style.input}
+        inputValue={inputValue}
+        freeSolo
+        onInputChange={(e: ChangeEvent, newInputValue: string) => {
+          setInputValue(newInputValue);
+        }}
+        options={inputOptions}
+        renderInput={(params: any) => <TextField {...params} label="Value" />}
+      />
+      <IconButton
+        type="submit"
+        aria-label="search"
+        className={style.iconButton}
+      >
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+}
---
+++ src/search/interfaces.ts
@@ -0,0 +1,5 @@
+export type Metadata = Array<MetaItem>;
+
+export type MetaItem = {
+  [index: string]: string | string[];
+};
