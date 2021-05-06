4fbdaf42bd8f17c8fbd62614c1b5c38a3c894a09
philipjackson <philiptgjackson@gmail.com>
2021-05-06 17:16:43 +0100

    fix: added MetadataDrawer.tsx

---
+++ src/MetadataDrawer.tsx
@@ -0,0 +1,61 @@
+import {
+  Drawer,
+  ListItem,
+  Typography,
+  ListItemText,
+  List,
+  Divider,
+  Toolbar,
+} from "@material-ui/core";
+import { MetaItem } from "@/search/interfaces";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import React, { ReactElement } from "react";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth, // it seems that this width is used for positioning and wrapping (increase it and the tiles will wrap as though the drawer was wider, but it won't render wider)
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth, // while this width is used for drawing the Drawer (increase it and the drawer will get wider but will draw over the tiles without wrapping them)
+    },
+  })
+);
+
+interface Props {
+  metadata: MetaItem;
+}
+
+export default function MetadataDrawer(props: Props): ReactElement {
+  const classes = useStyles();
+
+  return (
+    <Drawer
+      variant="permanent"
+      anchor="right"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Toolbar />
+      {/* This empty toolbar pushes the drawer contents down by the same thickness of the AppBard, so that they don't render behind it */}
+      {/* Moving the drawer itself down by placing a Toolbar above it seems not to work so instead we move its contents down and draw the AppBar on top of it */}
+      <ListItem>
+        <Typography variant="h4">Metadata</Typography>
+      </ListItem>
+      <Divider />
+
+      <List>
+        {Object.entries(props.metadata).map(([key, value], index) => (
+          <ListItem key={index}>
+            <ListItemText>{`${key}: ${value}`}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
