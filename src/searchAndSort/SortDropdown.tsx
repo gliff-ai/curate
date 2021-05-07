79195ccdd559a482ce005579f7d7ee6d9474a4a2
Silvia Zeni <silvia.szn@gmail.com>
2021-05-07 10:28:19 +0100

    feat: Sort by any metadata field (#26)
    * feat: Move image labels accordion inside left side drawer
    
    * feat: Sort WIP..
    
    * feat: Sort
    
    * fix: Add "All values" to search and tidy up
    
    * chore: Rename search and sort files
    
    * chore: tidy up
    
    * fix: Suggested changes

---
+++ src/searchAndSort/SortDropdown.tsx
@@ -0,0 +1,81 @@
+import React, { ReactElement, useEffect, useState, MouseEvent } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import SortIcon from "@material-ui/icons/Sort";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    menu: {
+      marginRight: theme.spacing(2),
+      marginTop: theme.spacing(5),
+    },
+  })
+);
+
+interface Props {
+  metadataKeys: string[];
+  inputKey: string;
+  callback: (key: string, sortOrder: string) => void;
+}
+
+export function SortDropdown({
+  metadataKeys,
+  inputKey,
+  callback,
+}: Props): ReactElement {
+  const style = useStyles();
+  type SortOrder = { [index: string]: string };
+
+  const initSortOrder = (): SortOrder => {
+    const initSort: SortOrder = {};
+    metadataKeys.forEach((key) => {
+      initSort[key] = null;
+    });
+    return initSort;
+  };
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(initSortOrder());
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSort = (value: string) => () => {
+    if (inputKey === "") return;
+    setSortOrder({ ...sortOrder, [inputKey]: value });
+  };
+
+  useEffect(() => {
+    callback(inputKey, sortOrder[inputKey]);
+  }, [sortOrder]);
+
+  return (
+    <div className={style.root}>
+      <IconButton
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <SortIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        className={style.menu}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleSort("asc")}>Sort by ASC</MenuItem>
+        <MenuItem onClick={handleSort("des")}>Sort by DESC</MenuItem>
+      </Menu>
+    </div>
+  );
+}
