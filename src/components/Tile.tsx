69aad02d6df344689e7349cf249a0dd29915fffa
philipjackson <philiptgjackson@gmail.com>
2021-05-13 18:06:20 +0100

    Upload (#37)
    * build(deps-dev): bump webpack-cli from 4.6.0 to 4.7.0
    
    Bumps [webpack-cli](https://github.com/webpack/webpack-cli) from 4.6.0 to 4.7.0.
    - [Release notes](https://github.com/webpack/webpack-cli/releases)
    - [Changelog](https://github.com/webpack/webpack-cli/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/webpack/webpack-cli/compare/webpack-cli@4.6.0...webpack-cli@4.7.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps-dev): bump webpack-cli from 4.6.0 to 4.7.0
    
    Bumps [webpack-cli](https://github.com/webpack/webpack-cli) from 4.6.0 to 4.7.0.
    - [Release notes](https://github.com/webpack/webpack-cli/releases)
    - [Changelog](https://github.com/webpack/webpack-cli/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/webpack/webpack-cli/compare/webpack-cli@4.6.0...webpack-cli@4.7.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: moved metadata loading into index.tsx, added Tile component
    
    * feat: added addUploadedImage callback in UserInterface
    
    * feat: added makeThumbnail, using it for uploaded images
    
    * fix lint errors
    
    * fix: lint errors
    
    * fix: remove unused import
    
    * chore: prettier
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Chas Nelson <cvs@chasnelson.co.uk>

---
+++ src/components/Tile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MetaItem } from "@/searchAndSort/interfaces";
+
+export default function Tile(props: { mitem: MetaItem }) {
+  return (
+    <canvas
+      width={128}
+      height={128}
+      ref={(canvas) => {
+        if (canvas) {
+          // Keep this as it is initially null
+          const canvasContext = canvas.getContext("2d");
+          if (props.mitem.thumbnail !== undefined) {
+            canvasContext.drawImage(props.mitem.thumbnail as ImageBitmap, 0, 0);
+          }
+        }
+      }}
+    />
+  );
+}
