f7e50e2d21356c96d347f885d2a5b9a1d5d7a9cb
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-06-20 01:57:16 +0100

    chore: replace hardcoded colours with colours defined in theme.ts

--- src/searchAndSort/SearchFilterCard.tsx
+++ src/searchAndSort/SearchFilterCard.tsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       height: "auto",
       marginBottom: "15px",
-      backgroundColor: "white",
+      backgroundColor: theme.palette.primary.light,
     },
 
     list: {
