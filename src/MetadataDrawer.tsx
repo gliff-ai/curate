597c5456bcdcfc50be7723132a1d3ba536a54c55
Craig Cooper <c@c9r.dev>
2021-07-02 11:51:12 +0100

    fix #98

--- src/MetadataDrawer.tsx
+++ src/MetadataDrawer.tsx
@@ -56,10 +56,24 @@ const useStyles = makeStyles({
   card: {
     backgroundColor: theme.palette.primary.light,
   },
-  metadata: {
-    fontWeight: 500,
-  },
   svgSmall: { width: "12px", height: "100%" },
+  metaKey: {
+    width: "100px",
+    "& > span": {
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    },
+  },
+  metaValue: {
+    width: "180px",
+    "& > span": {
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    },
+  },
+  metaList: {},
 });
 
 export const metadataNameMap: MetadataNameMap = {
@@ -67,8 +81,8 @@ export const metadataNameMap: MetadataNameMap = {
   size: "Size",
   dateCreated: "Created",
   dimensions: "Dimensions",
-  numberOfDimensions: "Number Of Dimensions",
-  numberOfChannels: "Number Of Channels",
+  numberOfDimensions: "# Dimensions",
+  numberOfChannels: "# Channels",
   imageLabels: "Labels",
 };
 
@@ -131,13 +145,15 @@ export default function MetadataDrawer(props: Props): ReactElement {
             {Object.keys(props.metadata)
               .filter((key) => Object.keys(metadataNameMap).includes(key))
               .map((key) => (
-                <ListItem key={key}>
-                  <ListItemText>
-                    <Typography
-                      className={classes.metadata}
-                    >{`${metadataNameMap[key]}:`}</Typography>
+                <ListItem key={key} className={classes.metaList}>
+                  <ListItemText
+                    primaryTypographyProps={{ variant: "h6" }}
+                    className={classes.metaKey}
+                  >
+                    {metadataNameMap[key]}:
                   </ListItemText>
-                  <ListItemText>
+
+                  <ListItemText className={classes.metaValue}>
                     {key === "imageLabels"
                       ? (props.metadata[key] as string[]).join(", ")
                       : props.metadata[key].toString()}
