b27444e2a57d33518cdf8d72a6433d11ac8d35ae
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-08-13 12:33:45 +0100

    feat: add padding to buttons

--- src/components/SizeThumbnails.tsx
+++ src/components/SizeThumbnails.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles({
     width: "150px",
     paddingLeft: "5px",
     paddingTop: "1px",
+    paddingBottom: "4px",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
