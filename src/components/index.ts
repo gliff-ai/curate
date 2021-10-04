47d15a334ef43e62ae0f4a1ab2bf026edcc4b670
Silvia Zeni <silvia.szn@gmail.com>
2021-10-04 13:59:37 +0100

    chore: Export components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { tooltips, thumbnailSizes } from "./Tooltips";
+export { SizeThumbnails } from "./SizeThumbnails";
+export { LabelsPopover } from "./LabelsPopover";
+
+import Tile from "./Tile";
+export default Tile;
