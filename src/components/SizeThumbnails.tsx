ff7b25cefbcfe41dfb09706942a792fce635eb8d
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-11-10 14:48:30 +0100

    feat: remove redundant css

--- src/components/SizeThumbnails.tsx
+++ src/components/SizeThumbnails.tsx
@@ -2,30 +2,13 @@ import { ReactElement, useState, useEffect } from "react";
 import { IconButton, theme } from "@gliff-ai/style";
 import { ButtonGroup } from "@material-ui/core";
 
-import { makeStyles } from "@material-ui/core/styles";
-
 import { thumbnailSizes, ThumbnailSizes } from "@/components/Tooltips";
-
-const useStyles = makeStyles({
-  svgSmall: { width: "22px", height: "100%" },
-  card: {
-    height: "48px",
-    backgroundColor: theme.palette.primary.light,
-    width: "150px",
-    paddingTop: "1px",
-    paddingBottom: "4px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 interface Props {
   resizeThumbnails: (size: number) => void;
 }
 
 export function SizeThumbnails({ resizeThumbnails }: Props): ReactElement {
-  const classes = useStyles();
   const [buttonClicked, setButtonClicked] = useState("Small Thumbnails");
 
   useEffect(() => {
