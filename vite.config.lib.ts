f13364c19136686f9d1d9f8a01e3c01cb9a5df9c
Christian Baidoo <37402968+ChrisBaidoo@users.noreply.github.com>
2022-02-03 14:32:52 +0000

    feat: Material UI v5 migrate (#320)
    * fix: use latest @gliff-ai/style
    
    * feat: update material ui packages
    
    * feat: run codemod
    
    * feat: add variant and size props
    
    * fix: lint errors
    
    * fix: lint errors
    
    * feat: use IconButton instead of BaseIconButton
    
    * feat: BREAKING CHANGE fix feedback from PR review
    
    * fix: lint errors
    
    * fix: percy visual testing
    
    * fix: feedback from PR review
    
    * feat: bump gliff-ai/style
    
    * fix: feedback from PR review
    
    * feat: fix assignee button
    
    * fix: assignee button
    
    * fix: remove border
    
    * feat: remove @mui/lab

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -17,10 +17,9 @@ export default defineConfig({
         "react",
         "react-dom",
         "react-router-dom",
-        "@material-ui/core",
-        "@material-ui/icons",
-        "@material-ui/lab",
-        "@gliff-ai/style"
+        "@mui/material",
+        "@mui/icons-material",
+        "@gliff-ai/style",
       ],
       output: {
         globals: {},
