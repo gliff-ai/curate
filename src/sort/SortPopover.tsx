84401dc7491d08a091eeb2de2063e0f09659aa0c
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-03-14 15:51:15 +0000

    feat: remove makeStyles

--- src/sort/SortPopover.tsx
+++ src/sort/SortPopover.tsx
@@ -1,66 +1,19 @@
 import { ReactElement, useEffect, useState, ChangeEvent } from "react";
-import { Paper, TextField, Checkbox } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
 import {
   BaseTextButton,
-  theme,
   BasePopover,
   RadioGroup,
   MenuItem,
+  Paper,
   FormControl,
+  TextField,
   FormControlLabel,
   Radio,
+  Checkbox,
   GliffPopover,
 } from "@gliff-ai/style";
 import { getLabelsFromKeys, MetadataLabel } from "@/search/SearchBar";
 import { tooltips } from "@/components/Tooltips";
-
-const useStyles = makeStyles({
-  card: {
-    backgroundColor: theme.palette.primary.light,
-    width: "260px",
-    height: "370px",
-    "& button:last-of-type": {
-      position: "absolute",
-      left: "50%",
-      transform: "translate(-50%, 0)",
-      bottom: "50px",
-    },
-  },
-  paperHeader: {
-    backgroundColor: theme.palette.primary.main,
-    width: "100%",
-    padding: "10px",
-    marginBottom: "10px",
-  },
-  paper: {
-    padding: "10px",
-    marginLeft: "15px",
-  },
-  paperPopover: {
-    margin: "0 15px",
-    padding: "5px",
-  },
-  typography: {
-    color: theme.palette.text.primary,
-    display: "inline",
-    fontSize: "21px",
-    marginLeft: "15px",
-  },
-
-  sortLabel: {
-    fontSize: "17px",
-  },
-  menuItem: {
-    backgroundColor: "#ffffff !important",
-  },
-  closeButton: {
-    position: "absolute",
-    top: "7px",
-    right: "5px",
-  },
-  closeIcon: { width: "15px" },
-});
 
 interface Props {
   metadataKeys: string[];
@@ -75,7 +28,6 @@ export const SortPopover = ({
   isGrouped,
   toggleIsGrouped,
 }: Props): ReactElement => {
-  const classes = useStyles();
   const [inputKey, setInputKey] = useState<MetadataLabel>({
     key: "",
     label: "",
@@ -124,18 +76,18 @@ export const SortPopover = ({
             >
               {metadataLabels &&
                 metadataLabels.map(({ key, label }) => (
-                  <MenuItem
-                    key={key}
-                    value={label}
-                    className={classes.menuItem}
-                  >
+                  <MenuItem key={key} value={label}>
                     {label}
                   </MenuItem>
                 ))}
             </TextField>
           </FormControl>
 
-          <Paper elevation={0} square className={classes.paper}>
+          <Paper
+            elevation={0}
+            square
+            style={{ padding: "10px", marginLeft: "15px" }}
+          >
             {/* Form for selecting a sort order */}
             <FormControl component="fieldset">
               <RadioGroup
@@ -148,17 +100,11 @@ export const SortPopover = ({
                   value="asc"
                   control={<Radio size="small" />}
                   label="Sort by ASC"
-                  classes={{
-                    label: classes.sortLabel,
-                  }}
                 />
                 <FormControlLabel
                   value="desc"
                   control={<Radio size="small" />}
                   label="Sort by DESC"
-                  classes={{
-                    label: classes.sortLabel,
-                  }}
                 />
               </RadioGroup>
             </FormControl>
