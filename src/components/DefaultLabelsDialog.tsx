a0870f58278b0602aee64e14ba26236516bd7b13
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-03-23 11:31:55 +0000

    feat: am import Dialog in DefautLabelsDialog

--- src/components/DefaultLabelsDialog.tsx
+++ src/components/DefaultLabelsDialog.tsx
@@ -1,8 +1,7 @@
 import { useState, ChangeEvent } from "react";
-import { IconButton } from "@mui/material"; // TODO import from STYLE
+// import { IconButton } from "@mui/material"; // TODO import from STYLE
 import SVG from "react-inlinesvg";
 import {
-  BaseIconButton,
   BaseTextButton,
   theme,
   icons,
@@ -12,7 +11,7 @@ import {
   Dialog,
   InputBase,
   Avatar,
-  GliffCard,
+  IconButton,
 } from "@gliff-ai/style";
 import { tooltips } from "./Tooltips";
 
@@ -66,156 +65,147 @@ export function DefaultLabelsDialog(props: Props): React.ReactElement {
   };
 
   return (
-    <>
-      <BaseIconButton
-        tooltip={tooltips.defaultLabels}
-        onClick={() => {
-          setOpen(!open);
-          // remember the original defaultLabels so we can revert if user hits Cancel:
-          setOldLabels(props.labels);
-          setOldRestrictLabels(props.restrictLabels);
-          setOldMultiLabel(props.multiLabel);
-        }}
-        tooltipPlacement="top"
-        id="set-default-labels"
-      />
-      <Dialog open={open} onClose={handleClose}>
-        <GliffCard
-          title="Set default labels"
-          el={
-            <div
-              style={{
-                width: "400px",
-                padding: "17px",
-                paddingTop: "10px",
-              }}
-            >
-              <InputBase
-                key="input-new-default-label"
-                placeholder="New label"
-                type="text"
-                value={inputString}
-                onChange={handleNewLabelChange}
-                onKeyPress={(e) => {
-                  if (e.key === "Enter") {
-                    handleAddLabel(inputString)();
-                  }
-                }}
-                size="small"
-                autoFocus
-              />
-              <IconButton
-                aria-label="add-label"
-                key="button-add-default-label"
-                style={{ position: "absolute", right: "10px" }}
-                onClick={handleAddLabel(inputString)}
-              >
-                <SVG
-                  src={icons.add}
-                  fill={theme.palette.text.secondary}
-                  width="15px"
-                />
-              </IconButton>
-              {props.labels.map((label) => (
-                <Chip
-                  key={`chip-add-${label}`}
-                  avatar={
-                    <Avatar
-                      variant="circular"
-                      style={{ cursor: "pointer" }}
-                      onClick={handleDeleteLabel(label)}
-                      data-testid={`delete-${label}`}
-                    >
-                      <SVG
-                        width="15px"
-                        src={icons.removeLabel}
-                        fill={theme.palette.text.secondary}
-                      />
-                    </Avatar>
-                  }
-                  label={label}
-                  variant="outlined"
-                />
-              ))}
-
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={props.restrictLabels}
-                    onChange={(event) => {
-                      props.updateDefaultLabels(
-                        props.labels,
-                        !props.restrictLabels,
-                        props.multiLabel,
-                        false
-                      );
-                    }}
-                  />
-                }
-                label="Restrict collaborators to these labels"
-                style={{ marginTop: "8px" }}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={props.multiLabel}
-                    onChange={(event) => {
-                      props.updateDefaultLabels(
-                        props.labels,
-                        props.restrictLabels,
-                        !props.multiLabel,
-                        false
-                      );
-                    }}
-                  />
-                }
-                label="Allow multiple labels per image"
-                style={{ marginTop: "8px" }}
-              />
-              <div
-                style={{
-                  display: "flex",
-                  marginTop: "20px",
-                  justifyContent: "space-between",
-                }}
-              >
-                <BaseTextButton
-                  id="cancel-default=labels"
-                  text="Cancel"
-                  onClick={() => {
-                    props.updateDefaultLabels(
-                      oldLabels,
-                      oldRestrictLabels,
-                      oldMultiLabel,
-                      false
-                    );
-                    handleClose();
-                  }}
-                  variant="outlined"
-                />
-                <BaseTextButton
-                  id="confirm-default-labels"
-                  text="Confirm"
-                  onClick={() => {
-                    props.updateDefaultLabels(
-                      props.labels,
-                      props.restrictLabels,
-                      props.multiLabel,
-                      true
-                    );
-                    handleClose();
-                  }}
-                />
-              </div>
-            </div>
-          }
-          action={{
-            onClick: () => {
-              handleClose();
-            },
+    <Dialog
+      title="Default Labels"
+      TriggerButton={
+        <IconButton
+          tooltip={tooltips.defaultLabels}
+          size="medium"
+          onClick={() => {
+            setOpen(!open);
+            // remember the original defaultLabels so we can revert if user hits Cancel:
+            setOldLabels(props.labels);
+            setOldRestrictLabels(props.restrictLabels);
+            setOldMultiLabel(props.multiLabel);
           }}
         />
-      </Dialog>
-    </>
+      }
+    >
+      <div
+        style={{
+          width: "400px",
+          padding: "17px",
+          paddingTop: "10px",
+        }}
+      >
+        <InputBase
+          key="input-new-default-label"
+          placeholder="New label"
+          type="text"
+          value={inputString}
+          onChange={handleNewLabelChange}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleAddLabel(inputString)();
+            }
+          }}
+          size="small"
+          autoFocus
+        />
+        <IconButton
+          aria-label="add-label"
+          key="button-add-default-label"
+          style={{ position: "absolute", right: "10px" }}
+          onClick={handleAddLabel(inputString)}
+        >
+          <SVG
+            src={icons.add}
+            fill={theme.palette.text.secondary}
+            width="15px"
+          />
+        </IconButton>
+        {props.labels.map((label) => (
+          <Chip
+            key={`chip-add-${label}`}
+            avatar={
+              <Avatar
+                variant="circular"
+                style={{ cursor: "pointer" }}
+                onClick={handleDeleteLabel(label)}
+                data-testid={`delete-${label}`}
+              >
+                <SVG
+                  width="15px"
+                  src={icons.removeLabel}
+                  fill={theme.palette.text.secondary}
+                />
+              </Avatar>
+            }
+            label={label}
+            variant="outlined"
+          />
+        ))}
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={props.restrictLabels}
+              onChange={(event) => {
+                props.updateDefaultLabels(
+                  props.labels,
+                  !props.restrictLabels,
+                  props.multiLabel,
+                  false
+                );
+              }}
+            />
+          }
+          label="Restrict collaborators to these labels"
+          style={{ marginTop: "8px" }}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={props.multiLabel}
+              onChange={(event) => {
+                props.updateDefaultLabels(
+                  props.labels,
+                  props.restrictLabels,
+                  !props.multiLabel,
+                  false
+                );
+              }}
+            />
+          }
+          label="Allow multiple labels per image"
+          style={{ marginTop: "8px" }}
+        />
+        <div
+          style={{
+            display: "flex",
+            marginTop: "20px",
+            justifyContent: "space-between",
+          }}
+        >
+          <BaseTextButton
+            id="cancel-default=labels"
+            text="Cancel"
+            onClick={() => {
+              props.updateDefaultLabels(
+                oldLabels,
+                oldRestrictLabels,
+                oldMultiLabel,
+                false
+              );
+              handleClose();
+            }}
+            variant="outlined"
+          />
+          <BaseTextButton
+            id="confirm-default-labels"
+            text="Confirm"
+            onClick={() => {
+              props.updateDefaultLabels(
+                props.labels,
+                props.restrictLabels,
+                props.multiLabel,
+                true
+              );
+              handleClose();
+            }}
+          />
+        </div>
+      </div>
+    </Dialog>
   );
 }
