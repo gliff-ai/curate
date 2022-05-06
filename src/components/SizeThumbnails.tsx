57f41c7afd10b44c5f14eae24485ab15ef74c60c
Christian Baidoo <37402968+ChrisBaidoo@users.noreply.github.com>
2022-05-06 13:57:06 +0100

    feat: Use dialog from style (#393)
    * fix: use latest @gliff-ai/style
    
    * feat: use Dialog from STYLE for DefaultLabelsDialog component
    
    * feat: remove makeStyles
    
    * feat: use Dialog from STYLE for AutoAssignDialog
    
    * feat: remove makeStyles
    
    * feat: remove makeStyles
    
    * feat: import components from @gliff-ai/style
    
    * feat: use forwardRef WIP
    
    * feat: import components from STYLE
    
    * feat: import components from STYLE
    
    * feat: pass down a prop to close Dialog
    
    * feat: add prop for closing Dialog
    
    * feat: re
    factor logic for closing  Dialog
    
    * feat: remove unnecessary state
    
    * feat: fix css on Alert component
    
    * feat:pass resetDefaults as prop
    
    * feat: bump @gliff-ai/style
    
    * feat: bump gliff-ai/style

--- src/components/SizeThumbnails.tsx
+++ src/components/SizeThumbnails.tsx
@@ -1,21 +1,6 @@
 import { ReactElement, useState, useEffect } from "react";
-import { IconButton, theme } from "@gliff-ai/style";
-import { ButtonGroup } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
+import { IconButton, ButtonGroup } from "@gliff-ai/style";
 import { thumbnailSizes, ThumbnailSizes } from "@/components/Tooltips";
-
-const useStyle = makeStyles({
-  card: {
-    backgroundColor: theme.palette.primary.light,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "48px",
-    width: "auto",
-    border: "none",
-  },
-  button: { padding: "5px" },
-});
 
 interface Props {
   resizeThumbnails: (size: number) => void;
@@ -23,7 +8,6 @@ interface Props {
 
 export function SizeThumbnails({ resizeThumbnails }: Props): ReactElement {
   const [buttonClicked, setButtonClicked] = useState("Small Thumbnails");
-  const classes = useStyle();
 
   useEffect(() => {
     thumbnailSizes.forEach((thumb) => {
@@ -34,14 +18,18 @@ export function SizeThumbnails({ resizeThumbnails }: Props): ReactElement {
   return (
     <ButtonGroup
       orientation="horizontal"
-      className={classes.card}
       variant="text"
+      sx={{
+        alignItems: "center",
+        height: "48px",
+        border: "none",
+      }}
     >
       {thumbnailSizes.map(({ name, icon, id }: ThumbnailSizes) => (
         <IconButton
           id={id}
           key={name}
-          className={classes.button}
+          sx={{ padding: "5px" }}
           icon={icon}
           tooltipPlacement="bottom"
           tooltip={{ name }}
