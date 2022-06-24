7e939065ee32328bdcfe5ee22d3d66d6a6218d38
Silvia Zeni <silvia.szn@gmail.com>
2022-06-24 11:20:20 +0100

    fix: Module export

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import UserInterface from "./ui";
 
 export type { MetaItem } from "./interfaces";
-export { Filters, FilterData, FilterDataItem } from "./filter";
+export { Filters } from "./filter";
+export type { FilterData, FilterDataItem } from "./filter";
 export { SearchBar, SearchFilterCard } from "./search";
 export { SortPopover } from "./sort";
 export default UserInterface;
