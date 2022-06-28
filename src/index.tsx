4cd22f75fb8f172e0a7e6efb519e0837dbadc269
Silvia Zeni <silvia.szn@gmail.com>
2022-06-28 10:26:14 +0100

    chore: Export getLabelsFromKeys

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ export type { MetaItem } from "./interfaces";
 export { Filters } from "./filter";
 export type { FilterData, FilterDataItem } from "./filter";
 export { SearchBar, SearchFilterCard } from "./search";
+export { getLabelsFromKeys } from "./helpers";
 export { SortPopover } from "./sort";
 export default UserInterface;
